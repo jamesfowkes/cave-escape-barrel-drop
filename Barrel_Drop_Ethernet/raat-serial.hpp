e6691a034401081992a496d756542a26ad349086
James Fowkes <jamesfowkes@gmail.com>
2019-10-06 22:48:04 +0100

    Remove autogenerated files from github

--- Barrel_Drop_Ethernet/raat-serial.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef _RAAT_SERIAL_H_
-#define _RAAT_SERIAL_H_
-
-typedef void (raat_serial_read_char_fn)(char c);
-
-void raat_serial_setup(long baudrate, raat_serial_read_char_fn& fn);
-
-#endif
